d1051d7b259f5bfc10491edac135a66066005280
Vladislav Uhwatov <vlad.uhwatov@yandex.ru>
2020-12-09 18:41:55 +0200

    Task 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,8 +5,5 @@ public class MainClass {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble(), b = scanner.nextDouble();
         System.out.printf("Ваши два действительных числа: %.2f и %.2f\nСумма чисел: %.2f\nРазность чисел: %.2f\nПроизведение чисел: %.2f\nЧастное чисел: %.2f.", a, b, a+b, a-b, a*b, a/b);
-
-
-
     }
 }
