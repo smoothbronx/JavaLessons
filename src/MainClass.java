bf9ec05b421301f2e32dc9b7dedf29d5ba3f6be1
Vladislav Uhwatov <vlad.uhwatov@yandex.ru>
2020-12-21 19:35:21 +0200

    Task

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,19 @@
 public class MainClass {
     public static void main(String[] args) {
-
+//    while (count >= 0){
+//        if (count % 100 == 0){
+//            System.out.println(count);
+//        }
+//        else {
+//            System.out.printf("%d ", count);
+//        }
+//        count--;
+//    }
+    for (int i=1; i<=9; i++) {
+        for (int j = 1; j <= 9; j++){
+            System.out.printf("%d x %d = %d\t", i, j, i*j);
+        }
+        System.out.println();
     }
-}
+}}
+
